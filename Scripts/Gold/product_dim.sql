bb272ce0f47c80877701a5347c266226628b68d1
sherif <83226602+sherif547@users.noreply.github.com>
2025-04-15 19:20:11 +0200

    Create product_dim.sql

---
+++ Scripts/Gold/product_dim.sql
@@ -0,0 +1,17 @@
+create view gold.dim_products as 
+SELECT
+	ROW_NUMBER()over(order by pn.prd_start_dt ,pn.prd_key) as product_key,
+    pn.prd_id AS product_id,
+    pn.prd_key AS product_number,
+    pn.prd_nm AS product_name,
+    pn.cat_id AS category_id,
+    pc.cat AS category,
+    pc.subcat AS subcategory,
+    pc.maintenance,
+    pn.prd_cost AS cost,
+    pn.prd_line AS product_line,
+    pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL  -- Filter out all historical data
